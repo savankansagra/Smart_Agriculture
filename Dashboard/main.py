af1df78ec5e89a55a8fafb04710f8a1370ae7056
savankansagra <kansagrasavan108@gmail.com>
2020-04-01 22:54:17 +0530

    change main.py form

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -12,19 +12,18 @@ def Crop_Prediction():
     if request.method == 'POST':
         data = {'City' : request.form["City"],
             'Year' : request.form["Year"],
-            'Season' : request.form["Crop"],
+            'Season' : request.form["Season"],
             'Crop' : request.form["Crop"],
-            'avgTemp' : request.form["Crop"],
-            'Cloud Cover' : request.form["Crop"],
-            'maxTemp' : request.form["Crop"],
-            'Precipitation' : request.form["Crop"],
-            'vapPressure' : request.form["Crop"],
-            'Rainfall' : request.form["Crop"],
-            'Wet Day Freq' : request.form["Crop"],
-            'minTemp' : request.form["Crop"]}
+            'avgTemp' : request.form["avgTemp"],
+            'Cloud Cover' : request.form["cloudcover"],
+            'maxTemp' : request.form["maxTemp"],
+            'Precipitation' : request.form["precipitation"],
+            'vapPressure' : request.form["vapPressure"],
+            'Rainfall' : request.form["Rainfall"],
+            'Wet Day Freq' : request.form["Wet Day Freq"],
+            'minTemp' : request.form["minTemp"]}
     pred = cyp.RFModel(data)
     return render_template('/home.html',pred = pred)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
