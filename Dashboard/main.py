384c07d0b1faa18c81431aaad9c779ed8616adcf
keyurkhant <kpkhant007@yahoo.com>
2020-04-05 17:49:17 +0530

    First Page By KP

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, url_for, redirect
 from Model_Package import CropYieldPrediction as cyp
+from Model_Package import WeatherData as wd
+
 
 app = Flask(__name__)
 
@@ -8,7 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/" , methods = ["POST"])
+@app.route("/prediction" , methods = ["POST"])
 def Crop_Prediction():
     if request.method == 'POST':
         data = {'City' : request.form["City"],
@@ -24,10 +26,23 @@ def Crop_Prediction():
             'Wet Day Freq' : int(request.form["Wet Day Freq"]),
             'minTemp' : int(request.form["minTemp"])}
     pred = cyp.RFModel(data)
-    return render_template('/home.html',pred = pred,data = data)
+    return render_template('/prediction.html',pred = pred,data = data)
+
+@app.route("/weather" , methods = ["POST"])
+def Weather():
+    if request.method == 'POST':
+        Name = request.form["Name"]
+        City = request.form["City"]
+        Email = request.form["Email"]
+        Area = float(request.form["Area"])
+    lst = [Name,City,Email,Area]
+    weatherdict = wd.FindWeather(City)
+    return render_template('/home.html',lst = lst,dict= weatherdict)
+
+@app.route("/p_redirect", methods = ["POST"])
+def prediction_redirect():
+    return render_template("prediction.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-print("my name is savan kasngra")
